53f3610d677498d63f4eb138afa70588b135739c
erikoros <erik.oros@availabill.ch>
2024-11-11 10:50:22 +0100

    dep update + user

--- src/main/java/com/example/userCRUD/UserCRUDApplication.java
+++ src/main/java/com/example/userCRUD/UserCRUDApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.userCRUD;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class UserCRUDApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(UserCRUDApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/userCRUD/user/User.java
@@ -0,0 +1,8 @@
+package com.example.userCRUD.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+}
