35d6d7d6fee8b1258e7392dce8d2201f65439895
thelittleerik <erik.oros1995@gmail.com>
2024-11-18 10:34:25 +0100

    Controller stuff

--- src/main/java/com/example/userCRUD/user/UserController.java
+++ src/main/java/com/example/userCRUD/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -29,5 +30,42 @@ public class UserController {
         return ResponseEntity.ok().build();
     }
 
+    @PostMapping("/new")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userRepository.save(user);
+        return ResponseEntity.ok().build();
+    }
 
+    @PutMapping("/{id}/edit")
+    public ResponseEntity<User> updateUser(@PathVariable UUID id, @RequestBody User user) {
+        Optional<User> existingUserOpt = userRepository.findById(id);
+
+        if (existingUserOpt.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        User existingUser = existingUserOpt.get();
+
+        if (user.getFirstname() != null) {
+            existingUser.setFirstname(user.getFirstname());
+        }
+        if (user.getLastname() != null) {
+            existingUser.setLastname(user.getLastname());
+        }
+        if (user.getAge() != 0) {
+            existingUser.setAge(user.getAge());
+        }
+        if (user.getHobbies() != null) {
+            existingUser.setHobbies(user.getHobbies());
+        }
+        existingUser.setSmoker(user.isSmoker());
+
+        if (user.getFavoriteFood() != null) {
+            existingUser.setFavoriteFood(user.getFavoriteFood());
+        }
+
+        userRepository.save(existingUser);
+
+        return ResponseEntity.ok(existingUser);
+    }
 }
