b1852dd89fef0de86ff8016b1da026f9cf7a2d9c
erikoros <erik.oros@availabill.ch>
2024-11-25 11:01:38 +0100

    imports deleted

--- src/main/java/com/example/userCRUD/config/OpenApiConfig.java
+++ src/main/java/com/example/userCRUD/config/OpenApiConfig.java
@@ -1,11 +1,9 @@
 package com.example.userCRUD.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
